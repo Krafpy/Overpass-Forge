48672cddc7d1331a8815d7dad70a1ce217164dee
Krafpy <nazar.misyats@gmail.com>
2023-08-07 02:25:56 +0200

    Basic query string beautifier

--- overpass_builder/utils.py
+++ overpass_builder/utils.py
@@ -1,4 +1,6 @@
 from typing import Iterable, Callable, TypeVar
+import re
+
 
 T = TypeVar('T')
 
@@ -10,3 +12,38 @@ def partition(pred: Callable[[T], bool], iterable: Iterable[T]) -> tuple[list[T]
         else:
             falses.append(item)
     return trues, falses
+
+
+def beautify(query: str) -> str:
+    """
+    Beautifies a compiled query string (i.e. adds indentation, line breaks...)
+    """
+    query, _ = re.subn(r"\(\(", "(\n(\n", query)
+    query, _ = re.subn(r"(\n\(|^\()", "\n(\n", query)
+    query, _ = re.subn(r"\n\n", "\n", query)
+    query, _ = re.subn(r"\)\;", "\n);", query)
+    query, _ = re.subn(r"\)\-\>", "\n)->", query)
+    query, _ = re.subn(r"\; ", ";\n", query)
+
+    indented = ""
+    indent = 0
+    i = 0
+    while i < len(query):
+        next_one = query[i]
+        next_two = query[i:min(i+2,len(query))]
+        if next_two == "(\n":
+            indent += 1
+            indented += "(\n" + "  " * indent
+            i += 2
+        elif next_two == "\n)":
+            indent -= 1
+            indented += "\n" + "  " * indent + ")"
+            i += 2
+        elif next_one == "\n":
+            indented += "\n" + "  " * indent
+            i += 1
+        else:
+            indented += query[i]
+            i += 1
+    
+    return indented
