7298bb3883eb4004368141e43315b5a479e06041
Krafpy <nazar.misyats@gmail.com>
2023-08-17 17:09:00 +0200

    Updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'Overpass Forge'
 copyright = '2023, Krafpy'
 author = 'Krafpy'
-release = '0.2.1'
+release = '0.3.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
